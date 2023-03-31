7ba00f26af3c56756244ea70ae61f26465cd399b
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2023-03-31 15:38:11 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f3f3f3",
+        secondary: "#333",
+        tertiary: "#0073E6",
+        baige: "#FEF8EC",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-bg": "url('./src/assets/bg-hero.png')",
+      },
+    },
+  },
+  plugins: [],
+};
